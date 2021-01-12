b7b0f5577ea46d7ee28161ab13b35a21d2566a8e
Mechztcc <dojathan@gmail.com>
2021-01-12 14:27:55 -0300

    Add a simple hover effect to itens list

--- src/components/MovieRow.jsx
+++ src/components/MovieRow.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
+import './MovieRow.css'
 
 function MovieRow({ title, items}) {
     return (
-        <div>
+        <div className="movieRow">
             <h2>{title}</h2>
             <div className="movieRow--listarea">
-                {items.results.length > 0 && items.results.map((item, key) => (
-                    <img key={key} src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt=""/>
-                ))}
+                <div className="movieRow--list">
+                    {items.results.length > 0 && items.results.map((item, key) => (
+                        <div key={key} className="movieRow--item">
+                            <img  src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt=""/>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     )
