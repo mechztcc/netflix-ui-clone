d558fef0f8df1804164330fc46f9c5f759dec384
Mechztcc <dojathan@gmail.com>
2021-01-13 13:26:08 -0300

    Try to update deploy

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,10 +5,10 @@ function Header({ black }) {
     return (
         <header className={black ? 'black' : ''}>
            <div className="header--logo">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Netflix_2014_logo.svg/300px-Netflix_2014_logo.svg.png"/>
+                <img alt="headerlogo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Netflix_2014_logo.svg/300px-Netflix_2014_logo.svg.png"/>
             </div>
             <div className="header--user">
-                <img src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"/>
+                <img alt="headeruser" src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"/>
             </div>
             
         </header>
